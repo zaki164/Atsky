3c4cf39cd6d4380f3febe8c16302076c2f7b0470
zaki <92805900+zaki164@users.noreply.github.com>
2022-12-31 13:39:35 +0200

    SecurityInfo Section

--- src/components/SecurityInfo/SecurityInfo.jsx
+++ src/components/SecurityInfo/SecurityInfo.jsx
@@ -1,4 +1,5 @@
 import './SecurityInfo.scss'
+import { ToggleCheckbox } from '../index' 
 
 const SecurityInfo = () => {
   return (
@@ -6,12 +7,26 @@ const SecurityInfo = () => {
       <h2>SecurityInfo</h2>
       <p className='p_custom'>Security Information About Your Account</p>
       <ul className='flex-column'>
-        <li className='flex_column'>
+        <li className='flex_between'>
           <div>
             <p className='fs_16 mb-2'>Password</p>
-            <p className='fs_16 p_custom'>Last Change On 25/10/2021</p>
+            <p className='mb-0 p_custom'>Last Change On 25/10/2021</p>
           </div>
           <button className='custom_btn'>Change</button>
+        </li>
+        <li className='flex_between'>
+          <div>
+            <p className='fs_16 mb-2'>Two-Factor Authentication</p>
+            <p className='mb-0 p_custom'>Enable/Disable The Feature</p>
+          </div>
+          <ToggleCheckbox ischecked={false} />
+        </li>
+        <li className='flex_between'>
+          <div>
+            <p className='fs_16 mb-2'>Devices</p>
+            <p className='mb-0 p_custom'>Check The Login Devices List</p>
+          </div>
+          <button className='custom_btn devices'>Devices</button>
         </li>
       </ul>
     </section>
