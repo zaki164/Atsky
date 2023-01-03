31f2967df1a9b436adae775c360d0e1b6da71a32
zaki <92805900+zaki164@users.noreply.github.com>
2023-01-03 16:10:40 +0200

    clean up libraries

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import './sass/App.scss';
 import Layout from "./Layout/Layout";
 import { BrowserRouter } from "react-router-dom";
-
-
-// <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
-// import 'aos/dist/aos.css';
-// import AOS from 'aos';
-// AOS.init({
-//   once: true,
-// });
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+AOS.init({
+  once: true,
+});
 
 
 const App = () => (
