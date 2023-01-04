29c1b67f59fd737b72850c6ae032f8be192c72c8
zaki <92805900+zaki164@users.noreply.github.com>
2023-01-04 17:34:49 +0200

    validate form

--- src/components/QuickDraft/QuickDraft.jsx
+++ src/components/QuickDraft/QuickDraft.jsx
@@ -1,13 +1,21 @@
+import { useRef } from 'react';
 import './QuickDraft.scss';
 
-const QuickDraft = () => {
+const QuickDraft = (e) => {
+  const titleRef = useRef()
+  const thoughtRef = useRef()
+  const handleSubmit = (e) => {
+    if (titleRef.current.value === '' || thoughtRef.current.value === '') {
+      e.preventDefault();
+    }
+  }
   return (
     <section className='QuickDraft'>
       <h2>Quick Draft</h2>
       <p className='p_custom'>Write A Draft For Your Ideas</p>
-      <form>
-        <input type="text" name='Title' placeholder='Title' />
-        <textarea name="Your Thought" placeholder='Your Thought' cols="30" rows="7"></textarea>
+      <form action='' onSubmit={handleSubmit}>
+        <input type="text" name='Title' placeholder='Title' ref={titleRef}/>
+        <textarea name="Your Thought" placeholder='Your Thought' cols="30" rows="7" ref={thoughtRef}></textarea>
         <input type="submit" value="Save" className='custom_btn ms-auto d-block' />
       </form>
     </section>
