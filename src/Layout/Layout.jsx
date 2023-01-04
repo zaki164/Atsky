a4058b162af4b861d738c6931191b26e741b1d1a
zaki <92805900+zaki164@users.noreply.github.com>
2023-01-04 16:59:45 +0200

    layout localstorage

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,22 +1,28 @@
 import { faRightLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRef } from "react";
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { Header, Sidebar } from "../components"
 import Router from "../Router/Router";
 import "./Layout.scss";
 
 const Layout = () => {
   const [small, setsmall] = useState(false);
-  const layoutRef = useRef();
+  const sidebarsmall = localStorage.getItem('sidebarSmall')
   const handleSizeSideBar = () => {
     setsmall(!small)
-    layoutRef.current.classList.toggle('smallSize');
+    localStorage.setItem('sidebarSmall', !small)
   }
+  useLayoutEffect(() => {
+    if (sidebarsmall) {
+      if (sidebarsmall === "true") {
+        setsmall(true);
+      }
+    }
+  }, [sidebarsmall])
   return (
     <>
       <Sidebar small={small} />
-      <div className="layout_content position-relative" ref={layoutRef}>
+      <div className={`layout_content position-relative ${small ? 'smallSize' : ''}`}>
         <span className="iconToggle position-absolute icon_shadow" onClick={handleSizeSideBar}>
           <FontAwesomeIcon icon={faRightLeft} />
         </span>
